14314d6ae4e8075096850d686cd6b2111305008e
Andy Wang <ownz.andy@gmail.com>
2016-12-12 22:06:11 -0500

    adding volunteer

--- app/modules/typeform_webhook.rb
+++ app/modules/typeform_webhook.rb
@@ -52,6 +52,8 @@ module TypeformWebhook
         parts << 'mentor'
       elsif part == 'Speaker'
         parts << 'speaker'
+      elsif part == 'Volunteer'
+        parts << 'volunteer'
       end
     end
     parts
